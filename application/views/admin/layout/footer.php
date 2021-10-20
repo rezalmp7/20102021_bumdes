c33cce556ddeca818b759e37aaf41428ed8867ea
rezalmp7 <rezalwahyu10@gmail.com>
2021-10-20 15:42:41 +0700

    [First Update]

---
+++ application/views/admin/layout/footer.php
@@ -0,0 +1,50 @@
+
+
+                <footer class="footer">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <script>document.write(new Date().getFullYear())</script> © Minia.
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="text-sm-end d-none d-sm-block">
+                                    Design & Develop by <a href="index.html#!" class="text-decoration-underline">Themesbrand</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+            <!-- end main content-->
+
+        </div>
+        <!-- END layout-wrapper -->
+
+        
+        <!-- Right bar overlay-->
+        <div class="rightbar-overlay"></div>
+
+        <!-- JAVASCRIPT -->
+        <script type="text/javascript" src="<?php echo base_url(); ?>assets/admin/assets/libs/jquery/jquery.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>assets/admin/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>assets/admin/assets/libs/metismenu/metisMenu.min.js"></script>
+        <script src="<?php echo base_url(); ?>assets/admin/assets/libs/simplebar/simplebar.min.js"></script>
+        <script src="<?php echo base_url(); ?>assets/admin/assets/libs/node-waves/waves.min.js"></script>
+        <script src="<?php echo base_url(); ?>assets/admin/assets/libs/feather-icons/feather.min.js"></script>
+        <!-- pace js -->
+        <script src="<?php echo base_url(); ?>assets/admin/assets/libs/pace-js/pace.min.js"></script>
+
+        <!-- apexcharts -->
+        <script src="<?php echo base_url(); ?>assets/admin/assets/libs/apexcharts/apexcharts.min.js"></script>
+
+        <!-- Plugins js-->
+        <script src="<?php echo base_url(); ?>assets/admin/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js"></script>
+        <script src="<?php echo base_url(); ?>assets/admin/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js"></script>
+        <!-- dashboard init -->
+        <script src="<?php echo base_url(); ?>assets/admin/assets/js/pages/dashboard.init.js"></script>
+
+        <script src="<?php echo base_url(); ?>assets/admin/assets/js/app.js"></script>
+
+    </body>
+
+</html>
